728d00a9982555f7f5b677e5177b8f98a292a908
ninja5tuna5 <shirleyhe2011@gmail.com>
2021-04-17 16:48:33 -0400

    More music

---
+++ Assets/Scripts/SettingMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;   
+
+    //Adapts volume level to slider value
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20 );
+        Debug.Log(volume);
+    }
+}
