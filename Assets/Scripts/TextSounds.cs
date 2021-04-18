b724ff99e183c67d5e35333b408e8417679fb165
ninja5tuna5 <shirleyhe2011@gmail.com>
2021-04-17 20:23:11 -0400

    Added UI Click Sounds

---
+++ Assets/Scripts/TextSounds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; 
+
+public class TextSounds : MonoBehaviour
+{
+    //When player starts typing, this sound is triggered 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UIEventSounds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIEventSounds : MonoBehaviour, IPointerClickHandler
+
+{
+    public AK.Wwise.Event OnPointerClickSound;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnPointerClickSound.Post(gameObject);
+    }
+}
